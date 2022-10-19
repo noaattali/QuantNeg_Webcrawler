91d2fcb18e7c9dc19b3de0bba45b1a33724d8afa
kyvinh <kyvinhmai@gmail.com>
2022-10-18 18:46:13 -0700

    Created section to determine if sentence is Quant neg or not. Also added write_csv_file.py

--- NPR_webscraper.py
+++ NPR_webscraper.py
@@ -1,22 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.npr.org/templates/story/story.php?storyId=19075588"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
-
-def grab_audio_link():
+def grab_audio_link(soup):
     """We can find the audio download button under wrapper > primary audio >
     audio-module-tools > audio-tool audio-tool-download ...."""
     audio_container = soup.find("li", class_ ="audio-tool audio-tool-download")
     attributes = audio_container.find("a")
     print(attributes['href'])
 
-def extract_transcript():
+def extract_transcript(soup):
     text_container = soup.find(class_ = "transcript storytext")
     text_tags = text_container.find_all("p")
     for lines in text_tags:
         print(lines.string)
 
 
-extract_transcript()
+def extract_metadata():
+    title
+    speakers
+    date
+
+
+
+if __name__ == "__main__":
+    url = "https://www.npr.org/templates/story/story.php?storyId=19075588"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import tqdm
-import pandas as pd
-import csv
 import requests
 from bs4 import BeautifulSoup
+import QuaNISC
+import NPR_webscraper
 
-
-story_links= []
-story_name = []
-match_case = []
-context = []
+clauses = 0
 
 
 hub_url = "https://www.npr.org/programs/all-things-considered/archive?date=12-31-2021"
 page = requests.get(hub_url)
 soup = BeautifulSoup(page.content, "html.parser")
 
-def validate_quant_neg(article_link):
-    pass
+def validate_quant_neg(article_link: str):
+    page = requests.get(article_link)
+    soup = BeautifulSoup(article_link)
+    sentences = extract_transcript(soup)
+    if QuaNISC.is_quantifier_negation(sentences):
+        identifications, indices = QuaNISC.find_quantifer_negation(sentences)
+        context = get_context(sentences, indices)
+        #grab audiofile
+        title, speakers, date = extract_metadata(soup)
+        clauses += clause_counter()
+
 
 
 def grab_day_links():#month_link: str):
